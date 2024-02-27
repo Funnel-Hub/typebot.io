7c336cd2c6c710592e05d66fc34b07af2ba898a3
henricker <henriquevieira@alu.ufc.br>
2024-02-27 14:01:25 -0300

    feat: load sidebar access dynamic by hub

--- apps/builder/src/components/FunnelHubSidebar.tsx
+++ apps/builder/src/components/FunnelHubSidebar.tsx
@@ -1,13 +1,39 @@
 import { ChakraSidebar } from '@funnelhub/sidebar'
-import { sidebarMenuData } from '@/lib/sidebar'
+import { env } from '@typebot.io/env'
+import { useSession } from 'next-auth/react'
 
-export const FunnelHubSidebar = ({ onClose }: { onClose?: () => void }) => (
-  <ChakraSidebar
-    onClose={() => {
-      onClose?.()
-    }}
-    display={{ base: 'none', md: 'block' }}
-    initialCollapsedState={true}
-    menuData={sidebarMenuData}
-  />
-)
+type sidebarConfigProps = {
+  userId: string
+  token: string
+}
+
+export function sidebarConfig({ token, userId }: sidebarConfigProps) {
+  return {
+    url: `${env.NEXT_PUBLIC_FUNNELHUB_URL}/api/v1/users/${userId}/profile`,
+    token,
+    method: 'GET',
+  }
+}
+
+export const FunnelHubSidebar = ({ onClose }: { onClose?: () => void }) => {
+  const session = useSession()
+
+  return (
+    <>
+      {session?.data?.user && (
+        <ChakraSidebar
+          onClose={() => {
+            onClose?.()
+          }}
+          display={{ base: 'none', md: 'block' }}
+          initialCollapsedState={true}
+          config={sidebarConfig({
+            token: session.data.user.apiToken,
+            userId: session.data.user.id,
+          })}
+          menuData={[]}
+        />
+      )}
+    </>
+  )
+}
---
+++ apps/builder/types/next-auth.d.ts
@@ -0,0 +1,16 @@
+import { DefaultSession } from "next-auth"
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session extends DefaultSession {
+    user: {
+      id: string
+      name: string
+      apiToken: string
+      email: string
+      image?: string
+    } 
+  }
+}
