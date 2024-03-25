f3cc091257f652bb0c3f32f74ae94c32a9a7063a
henricker <henriquevieira@alu.ufc.br>
2024-03-25 15:35:18 -0300

    fix: entrypoints

--- scripts/builder-entrypoint.sh
+++ scripts/builder-entrypoint.sh
@@ -4,4 +4,4 @@ npm install -g dotenv-cli
 cp .env ./apps/builder
 cd apps/builder;
 dotenv -e .env ./node_modules/.bin/prisma migrate deploy --schema=packages/prisma/postgresql/schema.prisma;
-dotenv -e .env HOSTNAME=0.0.0.0 PORT=3000 node apps/builder/server.js
+dotenv -e .env node apps/builder/server.js
--- scripts/viewer-entrypoint.sh
+++ scripts/viewer-entrypoint.sh
@@ -2,4 +2,4 @@
 npm install -g dotenv-cli
 cp .env ./apps/viewer
 cd apps/viewer
-dotenv -e .env HOSTNAME=0.0.0.0 PORT=3000 node apps/viewer/server.js
+dotenv -e node apps/viewer/server.js
